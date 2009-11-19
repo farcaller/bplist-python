2631c9ca4eb9ac2d199e495a43ca54b9b174df9f
Vladimir Pouzanov <farcaller@gmail.com>
2009-11-19 20:54:56 +0200

    added apple spec to be at hand

--- bplist.py
+++ bplist.py
@@ -5,6 +5,10 @@ class BPlistReader(object):
         self.data = s
     
     def __unpackIntStruct(self, sz, s):
+        '''__unpackIntStruct(size, string) -> int
+        
+        Unpacks the integer of given size (1, 2 or 4 bytes) from string
+        '''
         if   sz == 1:
             ot = '!B'
         elif sz == 2:
@@ -16,15 +20,27 @@ class BPlistReader(object):
         return struct.unpack(ot, s)[0]
     
     def __unpackInt(self, offset):
+        '''__unpackInt(offset) -> int
+        
+        Unpacks int field from plist at given offset
+        '''
         return self.__unpackIntMeta(offset)[1]
 
     def __unpackIntMeta(self, offset):
+        '''__unpackIntMeta(offset) -> (size, int)
+        
+        Unpacks int field from plist at given offset and returns its size and value
+        '''
         obj_header = struct.unpack('!B', self.data[offset])[0]
         obj_type, obj_info = (obj_header & 0xF0), (obj_header & 0x0F)
         int_sz = 2**obj_info
         return int_sz, self.__unpackIntStruct(int_sz, self.data[offset+1:offset+1+int_sz])
 
     def __resolveIntSize(self, obj_info, offset):
+        '''__resolveIntSize(obj_info, offset) -> (count, offset)
+        
+        Calculates count of objref* array entries and returns count and offset to first element
+        '''
         if obj_info == 0x0F:
             ofs, obj_count = self.__unpackIntMeta(offset+1)
             objref = offset+2+ofs
@@ -34,35 +50,48 @@ class BPlistReader(object):
         return obj_count, objref
 
     def __unpackItem(self, offset):
+        '''__unpackItem(offset)
+        
+        Unpacks and returns an item from plist
+        '''
         obj_header = struct.unpack('!B', self.data[offset])[0]
         obj_type, obj_info = (obj_header & 0xF0), (obj_header & 0x0F)
         if   obj_type == 0x00:
-            if   obj_info == 0x00:
+            if   obj_info == 0x00: # null   0000 0000
                 return None
-            elif obj_info == 0x08:
+            elif obj_info == 0x08: # bool   0000 1000           // false
                 return False
-            elif obj_info == 0x09:
+            elif obj_info == 0x09: # bool   0000 1001           // true
                 return True
-            elif obj_info == 0x0F:
+            elif obj_info == 0x0F: # fill   0000 1111           // fill byte
                 return None # this is really pad byte, FIXME
             else:
                 raise Exception('unpack item type '+str(obj_header)+' at '+str(offset)+ 'failed')
-        elif obj_type == 0x10:
+        elif obj_type == 0x10: #     int    0001 nnnn   ...     // # of bytes is 2^nnnn, big-endian bytes
             return self._unpackInt(offset)
-
-        # ...
-
-        elif obj_type == 0x50:
+        elif obj_type == 0x20: #    real    0010 nnnn   ...     // # of bytes is 2^nnnn, big-endian bytes
+            return # FIXME: implement
+        elif obj_type == 0x30: #    date    0011 0011   ...     // 8 byte float follows, big-endian bytes
+            return # FIXME: implement
+        elif obj_type == 0x40: #    data    0100 nnnn   [int]   ... // nnnn is number of bytes unless 1111 then int count follows, followed by bytes
+            return # FIXME: implement
+        elif obj_type == 0x50: #    string  0101 nnnn   [int]   ... // ASCII string, nnnn is # of chars, else 1111 then int count, then bytes
             obj_count, objref = self.__resolveIntSize(obj_info, offset)
             return self.data[objref:objref+obj_count]
-        # unicode str
-        elif obj_type == 0xA0:
+        elif obj_type == 0x60: #    string  0110 nnnn   [int]   ... // Unicode string, nnnn is # of chars, else 1111 then int count, then big-endian 2-byte uint16_t
+            return # FIXME: implement
+        elif obj_type == 0x80: #    uid     1000 nnnn   ...     // nnnn+1 is # of bytes
+            return # FIXME: implement
+        elif obj_type == 0xA0: #    array   1010 nnnn   [int]   objref* // nnnn is count, unless '1111', then int count follows
             obj_count, objref = self.__resolveIntSize(obj_info, offset)
             arr = []
             for i in range(obj_count):
                 arr.append(self.__unpackIntStruct(self.object_ref_size, self.data[objref+i:objref+i+self.object_ref_size]))
             return arr
-        elif obj_type == 0xD0:
+        elif obj_type == 0xC0: #   set      1100 nnnn   [int]   objref* // nnnn is count, unless '1111', then int count follows
+            # XXX: not serializable via apple implementation
+            return # FIXME: implement
+        elif obj_type == 0xD0: #   dict     1101 nnnn   [int]   keyref* objref* // nnnn is count, unless '1111', then int count follows
             obj_count, objref = self.__resolveIntSize(obj_info, offset)
             keys = []
             for i in range(obj_count):
